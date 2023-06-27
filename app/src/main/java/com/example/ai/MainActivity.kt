46a3ab1679adb7e79afc2ba7c850a0e644286f8c
JangWoojun <woojun0107@naver.com>
2023-06-27 23:36:44 +0900

    feat: Bottom Navigation 설정
    Bottom Navigation Fragment 이동 설정

--- app/src/main/java/com/example/ai/MainActivity.kt
+++ app/src/main/java/com/example/ai/MainActivity.kt
@@ -3,11 +3,35 @@ package com.example.ai
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.findNavController
+import com.example.ai.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         window.statusBarColor = Color.TRANSPARENT
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        binding.apply {
+            bottomNavigation.setItemSelected(R.id.home)
+
+            bottomNavigation.setOnItemSelectedListener {
+                when (it) {
+                    R.id.home -> navController.navigate(R.id.home)
+                    R.id.childrenInfo -> navController.navigate(R.id.childrenInfo)
+                    R.id.childrenList -> navController.navigate(R.id.childrenList)
+                    R.id.myInfo -> navController.navigate(R.id.myInfo)
+                }
+            }
+        }
+
     }
 }
