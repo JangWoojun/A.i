a6db66cbc72ed8f80fbe65a0f2e8716b116a0c76
JangWoojun <woojun0107@naver.com>
2023-06-28 15:47:53 +0900

    feat: ChildrenInfoAdapter 완성

---
+++ app/src/main/java/com/example/ai/util/ChildrenInfoAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.ai.util
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ai.R
+import com.example.ai.databinding.ChildrenInfoItemBinding
+
+class ChildrenInfoAdapter(private val children: ArrayList<ChildInfo>): RecyclerView.Adapter<ChildrenInfoAdapter.ChildrenInfoViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChildrenInfoViewHolder {
+        val binding = ChildrenInfoItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent, false
+        )
+        return ChildrenInfoViewHolder(binding).also { handler ->
+            binding.apply {
+
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+    override fun onBindViewHolder(holder: ChildrenInfoViewHolder, position: Int) {
+        holder.bind(children[position])
+    }
+
+    class ChildrenInfoViewHolder(private val binding: ChildrenInfoItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(childrenInfo: ChildInfo) {
+            binding.apply {
+                name.text = childrenInfo.name
+                image.setImageResource(R.drawable.profile)
+                sexBirthday.text = "${childrenInfo.sex} · ${childrenInfo.birthday.substringBefore("년")}년생"
+                location.text = childrenInfo.location
+                date.text = "${childrenInfo.date.substringAfter("년 ")} 실종"
+                age.text = "실종 나이 : ${childrenInfo.age}"
+            }
+        }
+    }
+}
