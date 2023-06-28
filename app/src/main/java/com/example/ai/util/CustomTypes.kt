a952846030b8b51184cd98a178ba582af85dfa62
JangWoojun <woojun0107@naver.com>
2023-06-28 15:44:52 +0900

    feat: Data Class 추가
    실종아동 정보를 담은 Data Class ChildInfo 생성

---
+++ app/src/main/java/com/example/ai/util/CustomTypes.kt
@@ -0,0 +1,18 @@
+package com.example.ai.util
+
+data class ChildInfo(
+    val name: String,
+    val image: String?,
+    val age: String,
+    val birthday: String,
+    val sex: String,
+    val date: String,
+    val location: String,
+    val height: Int,
+    val weight: Int,
+    val frame: String,
+    val faceType: String,
+    val hairColor: String,
+    val hairType: String,
+    val dress: String,
+)
