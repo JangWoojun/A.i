85449c6f3e582ef17168cf4074f4ea04f7dbe38f
JangWoojun <woojun0107@naver.com>
2023-06-30 10:47:22 +0900

    feat: ChildrenInfoAdapter 확장
    기존에 사용하던 NewChildrenInfoItem과 새로 추가한ChildrenInfoItem 모두 한 Adapter에서 처리할 수 있게 변경 및 ChildrenInfoItem 추가

--- app/src/main/java/com/example/ai/util/ChildrenInfoAdapter.kt
+++ app/src/main/java/com/example/ai/util/ChildrenInfoAdapter.kt
@@ -4,33 +4,82 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ai.R
+import com.example.ai.databinding.ChildrenInfoItemBinding
 import com.example.ai.databinding.NewChildrenInfoItemBinding
 
-class ChildrenInfoAdapter(private val children: ArrayList<ChildInfo>): RecyclerView.Adapter<ChildrenInfoAdapter.ChildrenInfoViewHolder>() {
+class ChildrenInfoAdapter(private val children: ArrayList<ChildInfo>, private val type: ChildInfoType): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return when (viewType) {
+            ChildInfoType.NEW.ordinal -> {
+                val binding = NewChildrenInfoItemBinding.inflate(inflater, parent, false)
+                NewChildrenInfoViewHolder(binding).also {
+                    binding.apply {
+                        newChildrenInfoItem.setOnClickListener {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChildrenInfoViewHolder {
-        val binding = NewChildrenInfoItemBinding.inflate(
-            LayoutInflater.from(parent.context),
-            parent, false
-        )
-        return ChildrenInfoViewHolder(binding).also { handler ->
-            binding.apply {
+                        }
+                    }
+                }
+            }
+            ChildInfoType.DEFAULT.ordinal -> {
+                val binding = ChildrenInfoItemBinding.inflate(inflater, parent, false)
+                ChildrenInfoViewHolder(binding).also {
+                    binding.apply {
+                        viewDetailsButton.setOnClickListener {
 
+                        }
+                    }
+                }
+            }
+            else -> throw IllegalArgumentException("Invalid view type")
+        }
+
+    }
+    override fun getItemCount(): Int {
+        return when (type) {
+            ChildInfoType.NEW -> {
+                3
+            }
+
+            ChildInfoType.DEFAULT -> {
+                children.size
+            }
+            else -> throw IllegalArgumentException("Invalid view type")
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return type.ordinal
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val childInfo = children[position]
+        when (holder) {
+            is NewChildrenInfoViewHolder -> {
+                holder.bind(childInfo)
+            }
+            is ChildrenInfoViewHolder -> {
+                holder.bind(childInfo)
             }
         }
     }
 
-    override fun getItemCount(): Int {
-        return 3
-    }
-
-    override fun onBindViewHolder(holder: ChildrenInfoViewHolder, position: Int) {
-        holder.bind(children[position])
-    }
-
-    class ChildrenInfoViewHolder(private val binding: NewChildrenInfoItemBinding) :
+    class NewChildrenInfoViewHolder(private val binding: NewChildrenInfoItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
+        fun bind(childrenInfo: ChildInfo) {
+            binding.apply {
+                name.text = childrenInfo.name
+                image.setImageResource(R.drawable.profile)
+                sexBirthday.text = "${childrenInfo.sex} · ${childrenInfo.birthday.substringBefore("년")}년생"
+                location.text = childrenInfo.location
+                date.text = "${childrenInfo.date.substringAfter("년 ")} 실종"
+                age.text = "실종 나이 : ${childrenInfo.age}"
+            }
+        }
+    }
 
+    class ChildrenInfoViewHolder(private val binding: ChildrenInfoItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun bind(childrenInfo: ChildInfo) {
             binding.apply {
                 name.text = childrenInfo.name
