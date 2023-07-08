183494cd271cac3515393690ddd3bd2251cf9e9c
JangWoojun <woojun0107@naver.com>
2023-07-08 15:21:30 +0900

    feat: LoginActivity로 이동 추가
    로그인 버튼에 LoginActivity로 이동 추가

--- app/src/main/java/com/woojun/ai/SignUpActivity.kt
+++ app/src/main/java/com/woojun/ai/SignUpActivity.kt
@@ -1,5 +1,6 @@
 package com.woojun.ai
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.woojun.ai.databinding.ActivitySignUpBinding
@@ -13,7 +14,9 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.apply {
-
+            moveLoginInButton.setOnClickListener {
+                startActivity(Intent(this@SignUpActivity, LoginActivity::class.java))
+            }
         }
     }
 }
