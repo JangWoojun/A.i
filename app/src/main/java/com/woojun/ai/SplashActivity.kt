97c21a5e3124ef21c854fdd486686719f0d2dbb2
JangWoojun <woojun0107@naver.com>
2023-07-13 18:05:42 +0900

    feat: 로그인 된 유저인 체크 추가
    로그인 되어있는 지 확인하여 AgreementActivity 또는 MainActivity로 이동하게 구현

--- app/src/main/java/com/woojun/ai/SplashActivity.kt
+++ app/src/main/java/com/woojun/ai/SplashActivity.kt
@@ -4,15 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        auth = Firebase.auth
 
         Handler().postDelayed({
-            startActivity(Intent(this, AgreementActivity::class.java))
-            finishAffinity()
+            if (auth.currentUser == null) {
+                startActivity(Intent(this, AgreementActivity::class.java))
+                finishAffinity()
+            } else {
+                startActivity(Intent(this, MainActivity::class.java))
+                finishAffinity()
+            }
         },1500)
     }
 }
