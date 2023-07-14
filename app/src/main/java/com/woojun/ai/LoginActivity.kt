d91f4140b921e21b1ade456cb53331c183cbd9ed
JangWoojun <woojun0107@naver.com>
2023-07-15 07:29:15 +0900

    feat: 이메일 로그인 구현
    Firebase auth를 사용하여 이메일 로그인 구현

--- app/src/main/java/com/woojun/ai/LoginActivity.kt
+++ app/src/main/java/com/woojun/ai/LoginActivity.kt
@@ -3,15 +3,24 @@ package com.woojun.ai
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.woojun.ai.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth = Firebase.auth
 
         textInputLayoutPaddingSetting()
 
@@ -24,7 +33,11 @@ class LoginActivity : AppCompatActivity() {
                 emailInputLayout.isErrorEnabled = false
                 passwordInputLayout.isErrorEnabled = false
 
-                validationCheck(emailArea.text.toString(), passwordArea.text.toString())
+                val loginCheck = validationCheck(emailArea.text.toString(), passwordArea.text.toString())
+
+                if (loginCheck) {
+                    loginUser(emailArea.text.toString(), passwordArea.text.toString())
+                }
             }
         }
     }
@@ -96,5 +109,22 @@ class LoginActivity : AppCompatActivity() {
     private fun isPasswordValid(password: String): Boolean {
         val passwordRegex = Regex("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@#$%^&+=!])(?=\\S+\$).{8,}\$")
         return passwordRegex.matches(password)
+    }
+
+    private fun loginUser(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this@LoginActivity, "로그인을 성공하셨습니다", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                    finishAffinity()
+                } else {
+                    try {
+                        throw task.exception!!
+                    } catch (e: Exception) {
+                        binding.emailInputLayout.error = "해당 이메일 또는 비밀번호가 유효하지 않습니다"
+                    }
+                }
+            }
     }
 }
