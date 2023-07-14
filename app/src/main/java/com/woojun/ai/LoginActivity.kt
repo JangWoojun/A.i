3acc5d298b4572f5435b4ef5d0a8b2e13e8c6538
JangWoojun <woojun0107@naver.com>
2023-07-15 07:15:45 +0900

    feat: 입력창 유효성 검사 추가
    로그인 버튼 클릭 시 입력창들 유효성 검사하기

--- app/src/main/java/com/woojun/ai/LoginActivity.kt
+++ app/src/main/java/com/woojun/ai/LoginActivity.kt
@@ -17,6 +17,46 @@ class LoginActivity : AppCompatActivity() {
             moveSignInButton.setOnClickListener {
                 startActivity(Intent(this@LoginActivity, SignUpActivity::class.java))
             }
+
+            loginButton.setOnClickListener {
+                validationCheck(emailArea.text.toString(), passwordArea.text.toString())
+            }
         }
+    }
+
+    private fun validationCheck(email: String, password: String): Boolean {
+        binding.apply {
+
+            if (email.isEmpty()) {
+                emailInputLayout.error = "이메일을 입력해주세요"
+                return false
+            } else if (!isEmailValid(email)) {
+                emailInputLayout.error = "유효한 이메일 형식이 아닙니다"
+                return false
+            }
+
+            if (password.isEmpty()) {
+                passwordInputLayout.error = "비밀번호를 입력해주세요"
+                return false
+            } else if (!isPasswordValid(password)) {
+                passwordInputLayout.error = "영문, 숫자, 특수문자 조합으로 8자 이상이 필요합니다"
+                return false
+            }
+
+            emailInputLayout.error = ""
+            passwordInputLayout.error = ""
+
+            return true
+        }
+    }
+
+    private fun isEmailValid(email: String): Boolean {
+        val emailRegex = Regex("""^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$""")
+        return emailRegex.matches(email)
+    }
+
+    private fun isPasswordValid(password: String): Boolean {
+        val passwordRegex = Regex("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@#$%^&+=!])(?=\\S+\$).{8,}\$")
+        return passwordRegex.matches(password)
     }
 }
