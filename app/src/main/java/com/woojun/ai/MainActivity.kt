cb8933f13c48aa66f60ee032569b6fa79057cb08
JangWoojun <woojun0107@naver.com>
2023-07-25 09:51:40 +0900

    test: API 호출 추가
    Retrofit2를 통한 API 호출 임시 세팅

--- app/src/main/java/com/woojun/ai/MainActivity.kt
+++ app/src/main/java/com/woojun/ai/MainActivity.kt
@@ -3,9 +3,16 @@ package com.woojun.ai
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import com.woojun.ai.databinding.ActivityMainBinding
+import com.woojun.ai.util.AiResultList
+import com.woojun.ai.util.RetrofitAPI
+import com.woojun.ai.util.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +21,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val retrofitAPI = RetrofitClient.getInstance().create(RetrofitAPI::class.java)
+        val call: Call<AiResultList> = retrofitAPI.getAiResult()
+
+        call.enqueue(object : Callback<AiResultList> {
+            override fun onResponse(call: Call<AiResultList>, response: Response<AiResultList>) {
+                if (response.isSuccessful) {
+                    val aiResultList: AiResultList? = response.body()
+                    aiResultList?.let {
+                        Log.d("확인", it.toString())
+                    }
+                } else {
+                    Log.d("확인1", "에러")
+                }
+            }
+
+            override fun onFailure(call: Call<AiResultList>, t: Throwable) {
+                Log.e("확인2", "API call failed: " + t.message);
+            }
+        })
 
         window.statusBarColor = Color.TRANSPARENT
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
