f86fa52c802e44132e04375266c4e6020ab5431f
JangWoojun <woojun0107@naver.com>
2023-07-28 13:35:04 +0900

    feat: 뒤로가기 시 HomeFragment로 이동하게 변경
    뒤로가기 버튼 클릭 시 HomFragment로 이동하고 그 후 한번 더 뒤로가기 버튼을 2초안에 두번 누를 시 종료되게 변경

--- app/src/main/java/com/woojun/ai/MainActivity.kt
+++ app/src/main/java/com/woojun/ai/MainActivity.kt
@@ -71,11 +71,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onBackPressed() {
-        if(System.currentTimeMillis() - backPressedTime >= 2000) {
-            backPressedTime = System.currentTimeMillis()
-            Toast.makeText(this, "한번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        if (navController.currentDestination?.id != R.id.home) {
+            navController.popBackStack(R.id.home, false)
         } else {
-            finish()
+            if (System.currentTimeMillis() - backPressedTime >= 2000) {
+                backPressedTime = System.currentTimeMillis()
+                Toast.makeText(this, "한 번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                finish()
+            }
         }
     }
 }
