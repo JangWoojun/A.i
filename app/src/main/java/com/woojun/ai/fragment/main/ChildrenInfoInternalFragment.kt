2b7279f42e78b02a2a0f2d9bd15d520beac9a12e
JangWoojun <woojun0107@naver.com>
2023-08-01 02:39:07 +0900

    feat: 전송받은 데이터 값 화면에 표시

--- app/src/main/java/com/woojun/ai/fragment/main/ChildrenInfoInternalFragment.kt
+++ app/src/main/java/com/woojun/ai/fragment/main/ChildrenInfoInternalFragment.kt
@@ -32,6 +32,24 @@ class ChildrenInfoInternalFragment : Fragment() {
         val mainActivity = activity as MainActivity
         mainActivity.hideBottomNavi(true)
 
+        binding.apply {
+            val bundle = arguments
+            val item = bundle?.getSerializable("child info") as ChildInfo
+
+            name.text = "이름: ${item.name}"
+            age1.text = "실종 나이: ${item.age1}"
+            age2.text = "현재 나이: ${item.age2}"
+            birthday.text = "생일: ${item.birthday}"
+            location.text = "지역: ${item.location}"
+            height.text = "키: ${item.height}"
+            weight.text = "몸무게: ${item.weight}"
+            frame.text = "체격: ${item.frame}"
+            faceType.text = "얼굴 특징: ${item.faceType}"
+            hairColor.text = "머리 색: ${item.hairColor}"
+            hairType.text = "머리 특징: ${item.hairType}"
+            dress.text = "실종 당시 복장: ${item.dress}"
+        }
+
     }
 
     override fun onDestroyView() {
