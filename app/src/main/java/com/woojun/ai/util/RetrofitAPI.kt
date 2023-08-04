7c016d01233b3fefbc8fedc11376d6c9544cb079
JangWoojun <woojun0107@naver.com>
2023-08-04 20:08:52 +0900

    feat: 호출 인터페이스 수정
    esntlId, authKey, rowSize, nm, age1, age2 파라미터 추가 및 GET 호출 주소 변경

--- app/src/main/java/com/woojun/ai/util/RetrofitAPI.kt
+++ app/src/main/java/com/woojun/ai/util/RetrofitAPI.kt
@@ -1,9 +1,19 @@
 package com.woojun.ai.util
 
+import com.woojun.ai.BuildConfig
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface RetrofitAPI {
-    @GET("find/")
-    fun getAiResult(): Call<AiResultList>
+    @GET(BuildConfig.GETURL)
+    fun getAiResult(
+        @Query("esntlId") esntlId: String,
+        @Query("authKey") authKey: String,
+        @Query("rowSize") rowSize: Int,
+        @Query("nm") nm: String?,
+        @Query("age1") age1: Int?,
+        @Query("age2") age2: Int?
+    ): Call<AiResultList>
+
 }
