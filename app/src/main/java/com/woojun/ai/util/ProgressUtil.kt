8f680a6a076fc949a80a9f977af002b467a8b699
JangWoojun <woojun0107@naver.com>
2023-08-11 23:08:04 +0900

    feat: ProgressUtil 함수 생성

---
+++ app/src/main/java/com/woojun/ai/util/ProgressUtil.kt
@@ -0,0 +1,22 @@
+package com.woojun.ai.util
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.Window
+import com.woojun.ai.R
+
+object ProgressUtil {
+
+    fun createLoadingDialog(context: Context): Dialog {
+        val dialog = Dialog(context)
+
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.setContentView(R.layout.activity_splash)
+
+        return dialog
+    }
+}
