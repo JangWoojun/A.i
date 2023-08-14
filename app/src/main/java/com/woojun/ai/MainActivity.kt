e2a0875693db88d46b38f5df984c49bcd114191d
JangWoojun <woojun0107@naver.com>
2023-08-14 18:16:50 +0900

    feat: 실종아동 정보 데이터 Fragment에 넘기 추가
    MainActivity에서 데이터를 호출하는 것이 아닌 SplashActivity에서 호출한 데이터를 받아 Fragment에 넘기는 방식으로 변경

--- app/src/main/java/com/woojun/ai/MainActivity.kt
+++ app/src/main/java/com/woojun/ai/MainActivity.kt
@@ -3,18 +3,10 @@ package com.woojun.ai
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import com.woojun.ai.databinding.ActivityMainBinding
-import com.woojun.ai.util.AiResultList
-import com.woojun.ai.util.RetrofitAPI
-import com.woojun.ai.util.RetrofitClient
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 
 class MainActivity : AppCompatActivity() {
@@ -26,39 +18,25 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val retrofitAPI = RetrofitClient.getInstance().create(RetrofitAPI::class.java)
-        val call: Call<AiResultList> = retrofitAPI.getAiResult(BuildConfig.ESNTLID, BuildConfig.AUTHKEY, 1, null, null, null)
-
-        call.enqueue(object : Callback<AiResultList> {
-            override fun onResponse(call: Call<AiResultList>, response: Response<AiResultList>) {
-                if (response.isSuccessful) {
-                    val aiResultList: AiResultList? = response.body()
-                    aiResultList?.let {
-                        Log.d("확인", it.toString())
-                    }
-                } else {
-                    Log.d("확인1", "에러")
-                }
-            }
-
-            override fun onFailure(call: Call<AiResultList>, t: Throwable) {
-                Log.e("확인2", "API call failed: " + t.message);
-            }
-        })
 
         window.statusBarColor = Color.TRANSPARENT
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         val navController = findNavController(R.id.nav_host_fragment)
 
+        val bundle = Bundle()
+        val item = intent.getStringExtra("item")
+        bundle.putString("item", item)
+        navController.navigate(R.id.home, bundle)
+
+
         binding.apply {
             bottomNavigation.setItemSelected(R.id.home)
-
             bottomNavigation.setOnItemSelectedListener {
                 when (it) {
-                    R.id.home -> navController.navigate(R.id.home)
+                    R.id.home -> navController.navigate(R.id.home, bundle)
                     R.id.childrenInfo -> navController.navigate(R.id.childrenInfo)
-                    R.id.childrenList -> navController.navigate(R.id.childrenList)
+                    R.id.childrenList -> navController.navigate(R.id.childrenList, bundle)
                     R.id.myInfo -> navController.navigate(R.id.myInfo)
                 }
             }
