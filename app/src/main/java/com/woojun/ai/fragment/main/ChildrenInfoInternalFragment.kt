497f115c9bea898d151a9b18092941a22242191c
JangWoojun <woojun0107@naver.com>
2023-08-14 21:37:08 +0900

    feat: Adapter로 전달된 데이터 바인딩

--- app/src/main/java/com/woojun/ai/fragment/main/ChildrenInfoInternalFragment.kt
+++ app/src/main/java/com/woojun/ai/fragment/main/ChildrenInfoInternalFragment.kt
@@ -5,10 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.addCallback
-import com.woojun.ai.MainActivity
 import com.woojun.ai.databinding.FragmentChildrenInfoInternalBinding
-import com.woojun.ai.util.ChildInfo
+import com.woojun.ai.util.AiResult
 
 class ChildrenInfoInternalFragment : Fragment() {
 
@@ -32,20 +30,19 @@ class ChildrenInfoInternalFragment : Fragment() {
 
         binding.apply {
             val bundle = arguments
-            val item = bundle?.getSerializable("child info") as ChildInfo
+            val item = bundle?.getParcelable<AiResult>("child info")
 
-            name.text = "이름: ${item.name}"
-            age1.text = "실종 나이: ${item.age1}"
-            age2.text = "현재 나이: ${item.age2}"
-            birthday.text = "생일: ${item.birthday}"
-            location.text = "지역: ${item.location}"
-            height.text = "키: ${item.height}"
-            weight.text = "몸무게: ${item.weight}"
-            frame.text = "체격: ${item.frame}"
-            faceType.text = "얼굴 특징: ${item.faceType}"
-            hairColor.text = "머리 색: ${item.hairColor}"
-            hairType.text = "머리 특징: ${item.hairType}"
-            dress.text = "실종 당시 복장: ${item.dress}"
+            if (item != null) {
+                name.text = "이름: ${item!!.nm}"
+                age1.text = "실종 나이: ${item.age}"
+                age2.text = "현재 나이: ${item.ageNow}"
+                sex.text = "성별: ${item.sexdstnDscd}"
+                date.text = "발생 일시: ${item.occrde}"
+                location.text = "지역: ${item.occrAdres}"
+                dress.text = "실종 당시 복장: ${item.alldressingDscd}"
+                type.text = "타입: ${item.writngTrgetDscd}"
+                characteristics.text = "특징: ${item.etcSpfeatr}"
+            }
         }
 
     }
