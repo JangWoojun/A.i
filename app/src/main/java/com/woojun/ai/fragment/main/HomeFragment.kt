caf62d25340549c6e78ff55bb682769cdf135001
JangWoojun <woojun0107@naver.com>
2023-08-15 04:35:43 +0900

    feat: SearchFragment 이동 추가

--- app/src/main/java/com/woojun/ai/fragment/main/HomeFragment.kt
+++ app/src/main/java/com/woojun/ai/fragment/main/HomeFragment.kt
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.gson.Gson
 import com.woojun.ai.MainActivity
@@ -48,6 +50,10 @@ class HomeFragment : Fragment() {
 
             mainChildrenInfoButton.setOnClickListener {
                 (activity as MainActivity).moveBottomNavigation(R.id.childrenInfo)
+            }
+
+            searchButton.setOnClickListener {
+                findNavController().navigate(R.id.action_home_to_searchFragment)
             }
 
             childrenInfoButton.setOnClickListener {
