cd0387522b0696f24e75a5361eb5f961570b6ece
JangWoojun <woojun0107@naver.com>
2023-08-14 18:36:28 +0900

    feat: 회원가입 함수 추가
    회원가입 시 필요한 로직을 실행하는 함수 signupMove 추가

--- app/src/main/java/com/woojun/ai/IntroActivity.kt
+++ app/src/main/java/com/woojun/ai/IntroActivity.kt
@@ -1,11 +1,20 @@
 package com.woojun.ai
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.woojun.ai.databinding.ActivityIntroBinding
+import com.woojun.ai.util.UserInfo
 
 class IntroActivity : AppCompatActivity() {
     private lateinit var binding: ActivityIntroBinding
+    private lateinit var database: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityIntroBinding.inflate(layoutInflater)
