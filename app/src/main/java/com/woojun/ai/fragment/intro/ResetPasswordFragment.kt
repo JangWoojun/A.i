218b3c545dc8090289d90d26d567234a939c6b6d
JangWoojun <woojun0107@naver.com>
2023-08-18 13:54:50 +0900

    feat: 비밀번호 재설정 링크 보내기 추가

--- app/src/main/java/com/woojun/ai/fragment/intro/ResetPasswordFragment.kt
+++ app/src/main/java/com/woojun/ai/fragment/intro/ResetPasswordFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.woojun.ai.databinding.FragmentResetPasswordBinding
 
 class ResetPasswordFragment : Fragment() {
@@ -28,12 +30,25 @@ class ResetPasswordFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.apply {
-
+            finishButton.setOnClickListener {
+                sendEmailForPasswordUpdate(emailArea.text.toString())
+            }
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun sendEmailForPasswordUpdate(email: String) {
+        val auth = FirebaseAuth.getInstance()
+        auth.sendPasswordResetEmail(email).addOnCompleteListener {
+            if(it.isSuccessful){
+                Toast.makeText(requireContext(),"이메일을 보냈습니다.",Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(requireContext(),"이메일 발송이 실패했습니다.",Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
