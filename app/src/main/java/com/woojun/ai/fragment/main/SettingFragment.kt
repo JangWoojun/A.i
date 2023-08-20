3904a998f620ea02e852f421b8dd330e3db5cd19
JangWoojun <woojun0107@naver.com>
2023-08-21 04:16:00 +0900

    feat: 로그아웃 기능 추가

--- app/src/main/java/com/woojun/ai/fragment/main/SettingFragment.kt
+++ app/src/main/java/com/woojun/ai/fragment/main/SettingFragment.kt
@@ -1,16 +1,23 @@
 package com.woojun.ai.fragment.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat.finishAffinity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.woojun.ai.IntroActivity
 import com.woojun.ai.databinding.FragmentSettingBinding
 
 class SettingFragment : Fragment() {
 
     private var _binding: FragmentSettingBinding? = null
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,9 +33,13 @@ class SettingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        auth = Firebase.auth
         binding.apply {
-
+            logoutButton.setOnClickListener {
+                auth.signOut()
+                startActivity(Intent(requireContext(), IntroActivity::class.java))
+                finishAffinity(requireActivity())
+            }
         }
     }
 
