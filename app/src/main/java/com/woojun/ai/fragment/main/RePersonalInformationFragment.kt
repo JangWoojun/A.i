c6e750488b7a8b2f8526e8b36cda66c7fc784aff
JangWoojun <woojun0107@naver.com>
2023-08-22 09:52:00 +0900

    fix: 잘못된 binding 수정

--- app/src/main/java/com/woojun/ai/fragment/main/RePersonalInformationFragment.kt
+++ app/src/main/java/com/woojun/ai/fragment/main/RePersonalInformationFragment.kt
@@ -1,17 +1,17 @@
 package com.woojun.ai.fragment.main
 
 import android.os.Bundle
+import android.text.InputType
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.woojun.ai.R
-import com.woojun.ai.databinding.FragmentResetPasswordBinding
-import com.woojun.ai.databinding.FragmentSettingBinding
+import androidx.appcompat.app.AlertDialog
+import com.woojun.ai.databinding.FragmentRePersonalInformationBinding
 
 class RePersonalInformationFragment : Fragment() {
 
-    private var _binding: FragmentResetPasswordBinding? = null
+    private var _binding: FragmentRePersonalInformationBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +22,7 @@ class RePersonalInformationFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentResetPasswordBinding.inflate(inflater, container, false)
+        _binding = FragmentRePersonalInformationBinding.inflate(inflater, container, false)
         return binding.root
     }
 
