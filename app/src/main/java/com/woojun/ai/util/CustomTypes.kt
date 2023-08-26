28bba86ca1022531a9f9d1421ee228aadf990f44
JangWoojun <woojun0107@naver.com>
2023-08-27 05:11:32 +0900

    feat: Onboard 관련 data class 생성

--- app/src/main/java/com/woojun/ai/util/CustomTypes.kt
+++ app/src/main/java/com/woojun/ai/util/CustomTypes.kt
@@ -3,16 +3,22 @@ package com.woojun.ai.util
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
+data class PagerItem(
+    val image: Int,
+    val title: String,
+    val subject: String
+)
+
 data class ResultSearchKeyword(
-    var documents: List<Place> // 검색 결과
+    var documents: List<Place>
 )
 
 data class Place(
-    var place_name: String, // 장소명, 업체명
-    var address_name: String, // 전체 지번 주소
-    var road_address_name: String, // 전체 도로명 주소
-    var x: String, // X 좌표값 혹은 longitude
-    var y: String, // Y 좌표값 혹은 latitude
+    var place_name: String,
+    var address_name: String,
+    var road_address_name: String,
+    var x: String,
+    var y: String,
 )
 
 data class UserInfo(
