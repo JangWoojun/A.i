a71af98c20d7929168fc56cf68140a85e7bd946e
JangWoojun <woojun0107@naver.com>
2023-08-30 23:23:28 +0900

    feat: AppDatabase 생성

---
+++ app/src/main/java/com/woojun/ai/util/AppDatabase.kt
@@ -0,0 +1,36 @@
+package com.woojun.ai.util
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@TypeConverters(
+    TypeConverter::class
+)
+
+@Database(entities = [UserInfo::class], version = 2)
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun userInfoDao(): UserInfoDAO
+
+    companion object {
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase? {
+            if (INSTANCE == null) {
+                INSTANCE = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app database")
+                    .fallbackToDestructiveMigration()
+                    .build()
+            }
+            return INSTANCE
+        }
+
+        fun destroyInstance() {
+            INSTANCE = null
+        }
+    }
+}
