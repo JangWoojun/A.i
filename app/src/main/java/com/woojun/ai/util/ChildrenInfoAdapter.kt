ffb92f9b70b1d2be0d78ad251d7bbc67ceb3f745
JangWoojun <woojun0107@naver.com>
2023-08-31 07:44:44 +0900

    feat: adapter 파라미터 private val children: MutableList<AiResult> 변경

--- app/src/main/java/com/woojun/ai/util/ChildrenInfoAdapter.kt
+++ app/src/main/java/com/woojun/ai/util/ChildrenInfoAdapter.kt
@@ -1,16 +1,22 @@
 package com.woojun.ai.util
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Base64
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DecodeFormat
+import com.bumptech.glide.request.RequestOptions
 import com.woojun.ai.R
 import com.woojun.ai.databinding.ChildrenInfoItemBinding
 import com.woojun.ai.databinding.NewChildrenInfoItemBinding
 import java.util.Calendar
 
-class ChildrenInfoAdapter(private val children: AiResultList, private val type: ChildInfoType): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class ChildrenInfoAdapter(private val children: MutableList<AiResult>, private val type: ChildInfoType): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         return when (viewType) {
