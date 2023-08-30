dd024b7658bc6496f751f395cc1d7bc578fc1a59
JangWoojun <woojun0107@naver.com>
2023-08-30 23:25:32 +0900

    feat: api 호출 삭제

--- app/src/main/java/com/woojun/ai/SplashActivity.kt
+++ app/src/main/java/com/woojun/ai/SplashActivity.kt
@@ -4,17 +4,10 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.os.Handler
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import com.google.gson.Gson
-import com.woojun.ai.util.AiResultList
-import com.woojun.ai.util.RetrofitAPI
-import com.woojun.ai.util.RetrofitClient
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class SplashActivity : AppCompatActivity() {
 
@@ -23,42 +16,19 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         auth = Firebase.auth
-
-        val retrofitAPI = RetrofitClient.getInstance().create(RetrofitAPI::class.java)
-        val call: Call<AiResultList> = retrofitAPI.getAiResult(BuildConfig.ESNTLID, BuildConfig.AUTHKEY, 10, null, null, null)
-
-        call.enqueue(object : Callback<AiResultList> {
-            override fun onResponse(call: Call<AiResultList>, response: Response<AiResultList>) {
-                if (response.isSuccessful) {
-                    val aiResultList: AiResultList? = response.body()
-                    aiResultList?.let {
-                        if (auth.currentUser == null) {
-                            val intent = Intent(this@SplashActivity, IntroActivity::class.java)
-                            val gson = Gson()
-                            intent.putExtra("item", gson.toJson(it))
-                            writeStringListToInternalStorage(baseContext, "search_text", arrayListOf())
-                            startActivity(intent)
-                            finishAffinity()
-                        } else {
-                            val intent = Intent(this@SplashActivity, MainActivity::class.java)
-                            val gson = Gson()
-                            intent.putExtra("item", gson.toJson(it))
-                            startActivity(intent)
-                            finishAffinity()
-                        }
-                    }
-                } else {
-                    Log.d("확인1", "에러")
-                }
+        Handler().postDelayed({
+            if (auth.currentUser == null) {
+                writeStringListToInternalStorage(baseContext, "search_text", arrayListOf())
+                startActivity(Intent(this@SplashActivity, IntroActivity::class.java))
+                finishAffinity()
+            } else {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                finishAffinity()
             }
-
-            override fun onFailure(call: Call<AiResultList>, t: Throwable) {
-                Log.e("확인2", "API call failed: " + t.message);
-            }
-        })
+        }, 2000)
     }
 
-    fun writeStringListToInternalStorage(context: Context, filename: String, stringList: ArrayList<String>) {
+    private fun writeStringListToInternalStorage(context: Context, filename: String, stringList: ArrayList<String>) {
         try {
             val outputStream = context.openFileOutput(filename, Context.MODE_PRIVATE)
             val fileContent = stringList.joinToString("\n")
