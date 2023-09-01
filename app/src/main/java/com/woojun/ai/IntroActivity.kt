ed9eb2502e378cedee8ae72c5a92cd4be004c999
JangWoojun <woojun0107@naver.com>
2023-09-01 13:17:54 +0900

    feat: 유저 로그인, 회원가입 시 room 데이터베이스 추가

--- app/src/main/java/com/woojun/ai/IntroActivity.kt
+++ app/src/main/java/com/woojun/ai/IntroActivity.kt
@@ -5,11 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.woojun.ai.databinding.ActivityIntroBinding
+import com.woojun.ai.util.AppDatabase
 import com.woojun.ai.util.UserInfo
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class IntroActivity : AppCompatActivity() {
     private lateinit var binding: ActivityIntroBinding
@@ -22,22 +29,50 @@ class IntroActivity : AppCompatActivity() {
     }
 
     fun loginMove() {
+        val auth = FirebaseAuth.getInstance()
+        database = Firebase.database.reference
+
         Toast.makeText(this, "로그인을 성공하셨습니다", Toast.LENGTH_SHORT).show()
-        val intent1 = Intent(this, MainActivity::class.java)
-        intent1.putExtra("item", intent.getStringExtra("item"))
-        startActivity(intent1)
-        finishAffinity()
+
+        database.child("users").child(auth.uid.toString()).addListenerForSingleValueEvent(object :
+            ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    val value = snapshot.getValue(UserInfo::class.java)
+
+                    CoroutineScope(Dispatchers.IO).launch {
+                        val db = AppDatabase.getDatabase(this@IntroActivity)
+                        val user = db!!.userInfoDao()
+
+                        user.insertUser(value!!)
+
+                        startActivity(Intent(this@IntroActivity, MainActivity::class.java))
+                        finishAffinity()
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
     }
 
     fun signupMove(userInfo: UserInfo) {
         val auth = FirebaseAuth.getInstance()
         database = Firebase.database.reference
 
-        database.child("users").child("${auth.uid}").setValue(userInfo)
         Toast.makeText(this, "회원가입을 성공하셨습니다", Toast.LENGTH_SHORT).show()
-        val intent2 = Intent(this, MainActivity::class.java)
-        intent2.putExtra("item", intent.getStringExtra("item"))
-        startActivity(intent2)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val db = AppDatabase.getDatabase(this@IntroActivity)
+            val userDao = db!!.userInfoDao()
+
+            userDao.insertUser(UserInfo(userInfo.name, userInfo.email, userInfo.phoneNumber, userInfo.check, userInfo.children))
+        }
+
+        database.child("users").child("${auth.uid}").setValue(userInfo)
+
+        startActivity(Intent(this, MainActivity::class.java))
         finishAffinity()
     }
 }
