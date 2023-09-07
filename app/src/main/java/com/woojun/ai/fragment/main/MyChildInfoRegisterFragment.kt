9ccfdb28f934ec6b00060f6c8ea1d2fd2671b4da
JangWoojun <woojun0107@naver.com>
2023-09-07 19:38:18 +0900

    feat: MyChildInfoRegisterFragment Bottom Navigation 숨기기

--- app/src/main/java/com/woojun/ai/fragment/main/MyChildInfoRegisterFragment.kt
+++ app/src/main/java/com/woojun/ai/fragment/main/MyChildInfoRegisterFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.findNavController
+import com.woojun.ai.MainActivity
 import com.woojun.ai.R
 import com.woojun.ai.databinding.FragmentMyChildInfoRegisterBinding
 
@@ -30,6 +31,9 @@ class MyChildInfoRegisterFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.apply {
+            val mainActivity = activity as MainActivity
+            mainActivity.hideBottomNavigation(true)
+
             cameraButton.setOnClickListener {
                 view.findNavController().navigate(R.id.action_myChildInfoRegisterFragment_to_cameraFragment)
             }
