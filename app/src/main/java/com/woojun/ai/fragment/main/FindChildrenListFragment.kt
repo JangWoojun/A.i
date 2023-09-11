315b7722e322e47643c64dcff840c2983a1d5511
JangWoojun <woojun0107@naver.com>
2023-09-11 13:18:32 +0900

    feat: 넘겨 받은 Bundle에 맞는 아이 정보 가져오기 추가

--- app/src/main/java/com/woojun/ai/fragment/main/FindChildrenListFragment.kt
+++ app/src/main/java/com/woojun/ai/fragment/main/FindChildrenListFragment.kt
@@ -5,12 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.woojun.ai.databinding.FragmentFindChildrenListBinding
+import com.woojun.ai.util.ChildInfo
+import com.woojun.ai.util.FindChildImageResult
+import com.woojun.ai.util.MyChildAdapterType
+import com.woojun.ai.util.MyChildInfoAdapter
+import com.woojun.ai.util.ProgressUtil
 
 class FindChildrenListFragment : Fragment() {
 
     private var _binding: FragmentFindChildrenListBinding? = null
     private val binding get() = _binding!!
+    private lateinit var database: DatabaseReference
+    private val list = arrayListOf<ChildInfo>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +33,7 @@ class FindChildrenListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentFindChildrenListBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -28,6 +42,34 @@ class FindChildrenListFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.apply {
+            val bundle = arguments
+            val similarDistanceUid = bundle?.getParcelable<FindChildImageResult>("children info")!!.similar_distance_uid
+
+            database = Firebase.database.reference
+
+            val loadingDialog = ProgressUtil.createLoadingDialog(requireContext())
+            loadingDialog.show()
+
+            similarDistanceUid.forEach {
+                database.child("children").child(it[0]).addListenerForSingleValueEvent(object :
+                    ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        if (snapshot.exists()) {
+                            val value = snapshot.getValue(ChildInfo::class.java)
+                            value?.let { it1 -> list.add(it1) }
+                            if (list.size == 5) {
+                                loadingDialog.dismiss()
+                                findChildrenList.layoutManager = LinearLayoutManager(requireContext().applicationContext)
+                                findChildrenList.adapter = MyChildInfoAdapter(list, MyChildAdapterType.Find)
+                            }
+                        }
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        loadingDialog.dismiss()
+                    }
+                })
+            }
 
         }
     }
