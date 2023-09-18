49043fdebbe853aa302cdb3ad425242308933a68
JangWoojun <woojun0107@naver.com>
2023-09-18 22:33:08 +0900

    feat: 회원탈퇴 시 비밀번호 입력 추가

--- app/src/main/java/com/woojun/ai/fragment/main/SettingFragment.kt
+++ app/src/main/java/com/woojun/ai/fragment/main/SettingFragment.kt
@@ -1,5 +1,6 @@
 package com.woojun.ai.fragment.main
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -7,14 +8,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.widget.Toast
+import androidx.cardview.widget.CardView
 import androidx.core.app.ActivityCompat.finishAffinity
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import com.woojun.ai.IntroActivity
 import com.woojun.ai.R
 import com.woojun.ai.databinding.FragmentSettingBinding
@@ -22,6 +28,7 @@ import com.woojun.ai.util.AppDatabase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SettingFragment : Fragment() {
 
@@ -64,26 +71,49 @@ class SettingFragment : Fragment() {
             }
 
             withdrawalButton.setOnClickListener {
-                val user = Firebase.auth.currentUser!!
 
-                user.delete()
-                    .addOnCompleteListener { task ->
+                showPasswordDialog { password ->
+                    val user = Firebase.auth.currentUser!!
+                    val credential = EmailAuthProvider.getCredential(user.email!!, password)
+
+                    user.reauthenticate(credential).addOnCompleteListener { task ->
                         if (task.isSuccessful) {
-                            database.child("users").child(user.uid).setValue(null)
-                            startActivity(Intent(requireContext(), IntroActivity::class.java))
-                            CoroutineScope(Dispatchers.IO).launch {
-                                val db = AppDatabase.getDatabase(requireContext())
-                                val userDao = db!!.userInfoDao()
-                                val findChildDao = db.findChildDao()
+                            user.delete()
+                                .addOnCompleteListener { task ->
+                                    if (task.isSuccessful) {
+                                        CoroutineScope(Dispatchers.IO).launch {
+                                            val db = AppDatabase.getDatabase(requireContext())
+                                            val userDao = db!!.userInfoDao()
+                                            val findChildDao = db.findChildDao()
 
-                                userDao.deleteUser(userDao.getUser())
-                                findChildDao.deleteFindChild(findChildDao.getFindChild())
-                            }
-                            finishAffinity(requireActivity())
+                                            val userChildrenList = userDao.getUser().children
+
+                                            userDao.deleteUser(userDao.getUser())
+                                            findChildDao.deleteFindChild(findChildDao.getFindChild())
+
+                                            withContext(Dispatchers.Main) {
+                                                database.child("users").child(user.uid).setValue(null)
+                                                userChildrenList.forEach {
+                                                    database.child("children").child(it.id).setValue(null)
+                                                    if (!deleteImageFromFirebaseStorage(it.photo)){
+                                                        Toast.makeText(requireContext(), "오류 발생 개발자에게 문의해주세요", Toast.LENGTH_SHORT).show()
+                                                    }
+                                                }
+                                                Toast.makeText(requireContext(), "회원탈퇴 완료", Toast.LENGTH_SHORT).show()
+                                                startActivity(Intent(requireContext(), IntroActivity::class.java))
+                                                finishAffinity(requireActivity())
+                                            }
+                                        }
+                                    } else {
+                                        Toast.makeText(requireContext(), "회원탈퇴를 실패하였습니다", Toast.LENGTH_SHORT).show()
+                                    }
+                                }
                         } else {
-                            Toast.makeText(requireContext(), "회원탈퇴를 실패하였습니다", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(requireContext(), "비밀번호가 잘못되었습니다", Toast.LENGTH_LONG).show()
                         }
                     }
+                }
+
             }
 
             rePersonalInformationButton.setOnClickListener{
@@ -91,7 +121,7 @@ class SettingFragment : Fragment() {
             }
 
             privacyPolicyButton.setOnClickListener {
-                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://sch10719.neocities.org/codingvocaprivacypolicy"))
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://yummy-slime-07b.notion.site/A-2a68a3b16e824ea3b91821695b611150?pvs=4"))
                 startActivity(intent)
             }
         }
@@ -100,6 +130,55 @@ class SettingFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun deleteImageFromFirebaseStorage(imageUrl: String): Boolean {
+        val storage = FirebaseStorage.getInstance()
+        val storageReference = storage.getReferenceFromUrl(imageUrl)
+
+        var isOk = true
+
+        storageReference.delete()
+            .addOnSuccessListener {
+            }
+            .addOnFailureListener { _ ->
+                isOk = false
+            }
+
+        return isOk
+    }
+
+    private fun showPasswordDialog(function: (password: String) -> Unit) {
+        val dialogView = layoutInflater.inflate(R.layout.password_request_item, null)
+        val passwordEditText = dialogView.findViewById<TextInputEditText>(R.id.password_area)
+        val submitButton = dialogView.findViewById<CardView>(R.id.finish_button)
+
+        val dialog = AlertDialog.Builder(requireContext())
+            .setView(dialogView)
+            .setCancelable(true)
+            .create()
+
+        dialog.show()
+
+        val layoutParams = WindowManager.LayoutParams()
+        val window = dialog.window
+        layoutParams.copyFrom(window?.attributes)
+        layoutParams.width = WindowManager.LayoutParams.MATCH_PARENT
+        layoutParams.height = WindowManager.LayoutParams.MATCH_PARENT
+        window?.attributes = layoutParams
+
+        dialog.window?.setBackgroundDrawableResource(R.drawable.dialog_background)
+
+        submitButton.setOnClickListener {
+            val password = passwordEditText.text.toString()
+            if (password.isNotEmpty()) {
+                function(password)
+                dialog.dismiss()
+            } else {
+                Toast.makeText(requireContext(), "비밀번호를 입력하세요.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 
 }
